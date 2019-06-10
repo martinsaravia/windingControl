df19b5cd06da67d7a14d98289d80f0da8e217c69
Martin <martinsaravia@gmail.com>
2019-06-10 19:02:48 +0000

    Add report file

--- windingControl.ino
+++ windingControl.ino
@@ -7,13 +7,29 @@ License
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    windingControl is distributed in the hope that it will be useful, but WITHOUT
     ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
     FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
     for more details.
 
     You should have received a copy of the GNU General Public License
-    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+    along with windingControl.  If not, see <http://www.gnu.org/licenses/>.
+    
+    
+    
+    
+                            Martín Saravia
+                            
+                Grupo de Investigación en Multifísica Aplicada
+    
+        Universidad Tecnológica Nacional - Facultad Regional Bahía Blanca
+    
+                                CONICET
+                                
+                                  2017
+    
+    
+    
 
 ------------------------------------------------------------*/
 
